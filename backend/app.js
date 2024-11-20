8e90a8262d1228ce489971cba152149cd97dd749
Artur Guerreiro <arturfontesguerreiro@gmail.com>
2024-11-20 18:56:47 +0000

    arrumando de novo

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,20 @@
 var express = require('express');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session');
 
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/auth');
 
 var app = express();
-
-app.use(session({
-    secret: 'FdzvG2o9cXl42OYocqurNNonhObVfaIf',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}));
-
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Authorization,X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    next();
-});
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/users', usersRouter);
-app.use('/api/auth', loginRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
